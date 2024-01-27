e266d8a7f1f4844df710d2373f856de03d049d08
bryanesteban <bryanesteban_123@hotmail.com>
2024-01-27 07:04:43 -0500

    authentication

---
+++ .history/src/main/java/com/bryan/backend/usersapp/backendusersapp/auth/SpringSecurityConfig_20240127064104.java
@@ -0,0 +1,17 @@
+package com.bryan.backend.usersapp.backendusersapp.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws{
+        return http.authorizeHttpRequests()
+        .requestMatchers(HttpMethod.GET, "/users").permitAll();
+    }
+}
