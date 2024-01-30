a1928a0023e06cb12e7c288d7aaab74934d0e76a
bryanesteban <bryanesteban_123@hotmail.com>
2024-01-29 19:57:20 -0500

    Spring security

---
+++ .history/src/main/java/com/bryan/backend/usersapp/backendusersapp/auth/filters/JwtAuthenticationFilter_20240129103510.java
@@ -0,0 +1,44 @@
+package com.bryan.backend.usersapp.backendusersapp.auth.filters;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        
+
+
+            
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        super.unsuccessfulAuthentication(request, response, failed);
+    }
+
+
+
+
+
+}
