aa8127a47bf6e4012c99b374163ac4e125fd19aa
bryanesteban <bryanesteban_123@hotmail.com>
2024-02-05 13:12:57 -0500

    jwt

--- .history/src/main/java/com/bryan/backend/usersapp/backendusersapp/auth/TokenJWTConfig_20240205131059.java
+++ .history/src/main/java/com/bryan/backend/usersapp/backendusersapp/auth/TokenJWTConfig_20240205131059.java
@@ -7,9 +7,10 @@ import io.jsonwebtoken.Jwts;
 public class TokenJWTConfig {
 
     //public final static String SECRET_KEY = "algun_token_con_alguna_frase_secreta";
-    public final static SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+    public final static String SECRET_KEY = "algun_token_con_alguna_frase_secreta";
     public final static String PREFIX_TOKEN = "Bearer ";
     public final static String HEADER_AUTHORIZATION = "Authorization";
 
+    SecretKey key = Jwts.SIG.HS256.key().build();
     
 }
